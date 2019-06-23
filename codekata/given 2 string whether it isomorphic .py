389008e157b3d292e09d82e2fb0fd8f4603b0a28
Muthu206balu <51689768+Muthu206balu@users.noreply.github.com>
2019-06-23 12:51:29 +0530

    Update given 2 string whether it isomorphic .py

--- codekata/given 2 string whether it isomorphic .py
+++ codekata/given 2 string whether it isomorphic .py
@@ -1,7 +1,7 @@
 get1,get2 = input().split()
 count = 0
 for k in range(len(get1)):
-  if get1.count1(get1[i]) == get2.count1(get2r2[i]):
+  if get1.count1(get1[k]) == get2.count1(get2[k]):
     count += 1
 if count == len(get1):
   print ("yes")
