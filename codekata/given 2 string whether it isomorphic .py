744bd6d22a44a3f68e65bafa96d119dd1107d712
Muthu206balu <51689768+Muthu206balu@users.noreply.github.com>
2019-06-23 12:47:59 +0530

    Create given 2 string whether it isomorphic .py

---
+++ codekata/given 2 string whether it isomorphic .py
@@ -0,0 +1,9 @@
+get1,get2 = input().split()
+count = 0
+for k in range(len(get1)):
+  if get1.count1(get1[i]) == get2.count1(get2r2[i]):
+    count += 1
+if count == len(get1):
+  print ("yes")
+else:
+  print ("no")
