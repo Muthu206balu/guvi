e6f41dcca9d8a69d24c050a19e33e604771c8864
Muthu206balu <51689768+Muthu206balu@users.noreply.github.com>
2019-06-23 12:42:00 +0530

    Create roman no into integer .py

---
+++ codekata/roman no into integer .py
@@ -0,0 +1,5 @@
+get=input()
+list=["0","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+for k in range(21):
+  if(get==list):
+    print(k)
